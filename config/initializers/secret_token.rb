adc221f6a664a3ae03e9bc2010ffb50dcdf6c1d8
Daniel Vydra <daniel.vydra@cogentconsulting.com.au>
2011-09-30 19:06:14 +1000

    moved rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirewallRules::Application.config.secret_token = '291062dac0531929903b51b55343e8f27a1a023ca59815033e693eceb4f1207c443308247568ccf7eeb2e8e781e6bd1ab16b0bdd2487bf152becda18e68e5c71'
